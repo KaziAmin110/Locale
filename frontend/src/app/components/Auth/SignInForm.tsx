7810ee67aa4fd401c0eb91c3dd29c8c3d3658289
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-09-19 18:13:52 -0400

    Fixed Sign In Button

--- frontend/src/app/components/Auth/SignInForm.tsx
+++ frontend/src/app/components/Auth/SignInForm.tsx
@@ -5,7 +5,7 @@ const SignInForm = () => {
     <div className="flex flex-col w-full mt-6 items-center justify-center gap-4">
       <Input placeholder="Email" />
       <Input placeholder="Password" />
-      <button className="w-full bg-[#333333] border-b-black text-white p-3 rounded-full mt-4 font-bold hover:bg-blue-600 transition">
+      <button className="w-full bg-[#333333] border-b-black text-white p-3 rounded-full mt-4 font-bold hover:cursor-pointer hover:bg-[#444444] hover:border-b-[#333333] transition">
         Sign In
       </button>
     </div>
