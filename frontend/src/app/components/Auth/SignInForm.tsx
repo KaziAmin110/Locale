a9b5046b67871eceb05bf7b623d3ef75a689d8ac
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-09-19 18:44:37 -0400

    Added Mock Functionality with API

--- frontend/src/app/components/Auth/SignInForm.tsx
+++ frontend/src/app/components/Auth/SignInForm.tsx
@@ -1,14 +1,47 @@
+"use client";
+import { useState } from "react";
 import Input from "../Form/Input";
 
 const SignInForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to sign in");
+      }
+
+      const data = await response.json();
+      console.log("Sign-in successful:", data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className="flex flex-col w-full mt-6 items-center justify-center gap-4">
-      <Input placeholder="Email" />
-      <Input placeholder="Password" />
-      <button className="w-full bg-[#333333] border-b-black text-white p-3 rounded-full mt-4 font-bold hover:cursor-pointer hover:bg-[#444444] hover:border-b-[#333333] transition">
+    <form
+      className="flex flex-col w-full mt-6 items-center justify-center gap-4"
+      onSubmit={handleSubmit}
+    >
+      <Input placeholder="Email" value={email} setValue={setEmail} />
+      <Input placeholder="Password" value={password} setValue={setPassword} />
+      <button
+        type="submit"
+        className="w-full bg-[#333333] border-b-black text-white p-3 rounded-full mt-4 font-bold hover:cursor-pointer hover:bg-[#444444] hover:border-b-[#333333] transition"
+      >
         Sign In
       </button>
-    </div>
+    </form>
   );
 };
 
--- frontend/src/app/components/Form/Input.tsx
+++ frontend/src/app/components/Form/Input.tsx
@@ -1,8 +1,18 @@
-const Input = ({ placeholder }: { placeholder: string }) => {
+const Input = ({
+  placeholder,
+  value,
+  setValue,
+}: {
+  placeholder: string;
+  value: string;
+  setValue: (value: string) => void;
+}) => {
   return (
     <input
-      className="w-full p-3 text-gray-800 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+      className="w-full p-3 text-gray-800 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-black"
       placeholder={placeholder}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
     ></input>
   );
 };
