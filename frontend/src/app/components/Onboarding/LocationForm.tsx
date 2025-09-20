bdc15988ffe37bf447f5e5d2da3a53c5832a5675
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-09-19 23:15:15 -0400

    Added Location Design Changes

--- frontend/src/app/components/Onboarding/LocationForm.tsx
+++ frontend/src/app/components/Onboarding/LocationForm.tsx
@@ -113,7 +113,7 @@ const LocationForm = ({
     <div className="flex-1 flex flex-col justify-evenly w-full max-w-lg px-4 pt-8 pb-4">
       <div className="flex flex-col gap-6">
         <div className="flex flex-col gap-3">
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between items-center mb-2">
             <label htmlFor="location" className="text-lg text-black">
               Location:
             </label>
