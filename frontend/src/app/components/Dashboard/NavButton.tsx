72e2900e8e2f44d0589ba2b73bba4d69f6506e61
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-09-20 12:54:42 -0400

    Added Base NavButton design

--- frontend/src/app/components/Dashboard/NavButton.tsx
+++ frontend/src/app/components/Dashboard/NavButton.tsx
@@ -12,13 +12,13 @@ const NavButton = ({
 }) => {
   return (
     <button
-      className={`flex items-center gap-2 p-2 text-black ${
+      className={`flex items-center gap-2 p-2 text-black hover:cursor-pointer ${
         isSelected
-          ? "underline font-semibold"
-          : "text-gray-600 hover:text-gray-800"
+          ? "border-b-3 border-green-600 font-semibold"
+          : "text-gray-600 hover:text-gray-800 font-semibold  hover:scale-105 transition-transform duration-200"
       }`}
     >
-      <Image src={imageSrc} alt={text} width={40} height={40} />
+      <Image src={imageSrc} alt={text} width={45} height={45} />
       <span>{text}</span>
     </button>
   );
